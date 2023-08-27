2b2a25f3ab5dc6dc9707ffb6965f2dc91a4b3045
Sirawich Rattanasopha <140170380+SirawichHim@users.noreply.github.com>
2023-08-27 20:27:29 +0700

    Update Autocopy_paste_word.py

--- python/Autocopy_paste_word.py
+++ python/Autocopy_paste_word.py
@@ -1,55 +1,64 @@
-# 1st You have to run "pip install pyautogui pyperclip" on command prompt
-# 2nd You can change the name "file.txt" to something else in line 7, such as "keyword.txt" like I did.
+OBJECTIVE : i'm lazy to copy paste and enter for query data on my company tool
 import time
 import pyautogui
 import pyperclip
+import tkinter as tk
+from threading import Thread
 
-with open('keyword.txt', 'r', encoding='utf-8') as file:
-    word_list = file.read().splitlines()
-
-# Setting time to switch to the target window : Recommend 5 
-time.sleep(5)
-
-# Loop through the words and automate the process
-for word in word_list:
-    # Copy word to clipboard
-    pyperclip.copy(word)
-    
-    # Simulate paste and enter
-    pyautogui.hotkey('ctrl', 'v')
-    pyautogui.press('enter')
-    
-    # Setting time for pasting next word
-    time.sleep(1)
-
-# Actually, You can add stop function through "ESC" but you have to install keyboard on command prompt "pip install keyboard" 
-import time
-import pyautogui
-import pyperclip
-import keyboard
-
-with open('keyword.txt', 'r', encoding='utf-8') as file:
-    word_list = file.read().splitlines()
-
-# Setting time to switch to the target window : Recommend 5
-time.sleep(5)
-
-# Loop through the words and automate the process
-try:
-    for word in word_list:
-        if keyboard.is_pressed('Esc'):  # You can change to others
-            print("Script stopped by user.")
-            break
-
-        # Copy word to clipboard
-        pyperclip.copy(word)
+class AutoKeywordApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Program_name")
         
-        # Simulate paste and enter
-        pyautogui.hotkey('ctrl', 'v')
-        pyautogui.press('enter')
+        self.file_name_entry = tk.Entry(root)
+        self.file_name_entry.pack()
         
-       # Setting time for pasting next word
-        time.sleep(1)
-except Exception as e:
-    print("An error occurred:", e)
+        self.start_button = tk.Button(root, text="Start (Enter)", command=self.start_automation)
+        self.start_button.pack()
+        
+        self.pause_button = tk.Button(root, text="Pause (ESC)", command=self.pause_automation)
+        self.pause_button.pack()
+        
+        self.word_list = []
+        
+        self.root.bind("<Return>", lambda event: self.start_automation())  # Bind Enter key
+        self.root.bind("<Escape>", lambda event: self.pause_automation())  # Bind ESC key
 
+    def start_automation(self):
+        self.running = True
+        self.paused = False
+        self.read_keywords()
+        self.automation_thread = Thread(target=self.run_automation)
+        self.automation_thread.start()
+
+    def pause_automation(self):
+        self.paused = not self.paused
+
+    def read_keywords(self):
+        file_name = self.file_name_entry.get()
+        print(f"Reading keywords from file: {file_name}")  # Debug line
+        self.word_list = []  
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                self.word_list = file.read().splitlines()
+            print(f"Loaded {len(self.word_list)} keywords from {file_name}")
+        except FileNotFoundError:
+            print(f"File {file_name} not found")
+
+    def run_automation(self):
+        time.sleep(5)  
+        for word in self.word_list:
+            if not self.running:
+                break
+            if self.paused:
+                while self.paused:
+                    time.sleep(1)
+            pyperclip.copy(word)
+            pyautogui.hotkey('ctrl', 'v')
+            pyautogui.press('enter')
+            time.sleep(1)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AutoKeywordApp(root)
+    root.mainloop()
